802b4b860e265f12b21ab307e0657c7c19f5b4ea
Simon Zwick <simonzwick19@gmail.com>
2017-01-12 16:43:14 -0800

    Added constructor with surname, first name, email and grades.
    Also added 2 mutator functions one for email and the other for grade.

--- Student.php
+++ Student.php
@@ -11,14 +11,21 @@
  *
  * @author Zwick
  */
-class Student {
-    
-    
+class Student {   
     function __construct() {
     $this->surname = '';
     $this->first_name = '';
     $this->emails = array();
     $this->grades = array();
 }
-    
+
+function add_email($which,$address) {
+    $this->emails[$which] = $address;
+}
+
+function add_grade($grade) {
+    $this->grades[] = $grade;
+}    
+
+
 }
